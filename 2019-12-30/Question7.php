a2550908a73cc1a16f01bcd3170939c7f28b20c6
Victor Tang <tanghengzhi@live.com>
2020-01-05 23:28:53 +0800

    数对之差的最大值

---
+++ 2019-12-30/Question7.php
@@ -0,0 +1,15 @@
+<?php
+
+$array = [2, 4, 1, 16, 7, 5, 11, 9];
+
+$max = 0;
+
+for($i = 0; $i < count($array) - 1; $i++) {
+    for ($j = $i + 1; $j < count($array); $j++) {
+        if ($array[$i] - $array[$j] > $max) {
+            $max = $array[$i] - $array[$j];
+        }
+    }
+}
+
+echo $max, "\n";
