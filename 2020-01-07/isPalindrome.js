f6482ff40ef8c73882be265934d4e726ee6ad577
Victor Tang <tanghengzhi@live.com>
2020-01-28 22:52:43 +0800

    fix issues

--- 2020-01-07/isPalindrome.js
+++ 2020-01-07/isPalindrome.js
@@ -3,5 +3,6 @@ console.log(isPalindrome("levels"));
 console.log(isPalindrome("A car, a man, a maraca"));
 
 function isPalindrome(str) {
+    str = str.replace(/\W/g, "").toLowerCase();
     return str.split("").reverse().join("") == str;
 }
