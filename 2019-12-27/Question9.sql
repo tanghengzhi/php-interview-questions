651c32b2bb3cf31de03c61485d43d6f4d49286a3
Victor Tang <tanghengzhi@live.com>
2020-01-30 22:52:01 +0800

    subquery

--- 2019-12-27/Question9.sql
+++ 2019-12-27/Question9.sql
@@ -2,3 +2,7 @@ select department.name, substring_index(group_concat(employee.name order by sala
 from employee 
 join department on department.id = employee.departmentId 
 group by department.id;
+
+# subquery
+select name, (select group_concat(employee.name) from (select * from employee where employee.departmentId = department.id order by salary desc limit 3) as employee) 
+from department;
